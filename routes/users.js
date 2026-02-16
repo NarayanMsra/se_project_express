7864bec86526d37d96fb7f7e5a2a34933e422d9e
Narayan Msra <arayannmsra@MacBook-Pro.local>
2026-02-15 23:33:25 -0500

    project

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,9 @@
 const router = require("express").Router();
 const auth = require("../middlewares/auth");
-const {
-  createUser,
-  updateUser,
-  login,
-  getUser,
-} = require("../controllers/users");
+const { createUser, updateUser, getUser } = require("../controllers/users");
 
 // signup
 router.post("/", createUser);
-
-// Login
-router.post("/login", login);
 
 // Get current user info and auth
 router.get("/me", auth, getUser);
